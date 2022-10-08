e1468f9168d9ae81a145a1bb9a767db44b776448
Toth Mihaly <98757199+sze1totmih@users.noreply.github.com>
2022-10-08 13:51:34 +0200

    Add files via upload

---
+++ autoverseny/AutoVerseny/AutoVerseny/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AutoVerseny
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Versenyzo> autoverseny = new List<Versenyzo>();
+            #region 2. feladat
+            using (var sr = new StreamReader("autoverseny.csv"))
+            {
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    string[] sor = sr.ReadLine().Split(';');
+                    autoverseny.Add(new Versenyzo(sor));
+                }
+            }
+            #endregion
+
+            #region 3. feladat
+            Console.WriteLine("3. feladat: " + autoverseny.Count);
+            #endregion
+
+            #region 4. feladat
+            var FF_GPX_3 = autoverseny.SingleOrDefault(x => x.VersenyzoNeve.Equals("Fürge Ferenc") && x.Palya.Equals("Gran Prix Circuit") && x.Kor.Equals(3)).Korido;
+            Console.WriteLine("4. feladat: " + FF_GPX_3.TotalSeconds + " másodperc");
+            #endregion
+
+            #region 5. feladat
+            Console.WriteLine("5. feladat: \n Kérem egy versenyző nevét:");
+            #endregion
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ autoverseny/AutoVerseny/AutoVerseny/Versenyzo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoVerseny
+{
+    class Versenyzo
+    {
+        public string Csapat { get; set; }
+        public string VersenyzoNeve { get; set; }
+        public int Eletkor { get; set; }
+        public string Palya { get; set; }
+        public TimeSpan Korido { get; set; }
+        public int Kor { get; set; }
+
+        public Versenyzo(string csapat, string versenyzoNeve, int eletkor, string palya, TimeSpan korido, int kor)
+        {
+            Csapat = csapat;
+            VersenyzoNeve = versenyzoNeve;
+            Eletkor = eletkor;
+            Palya = palya;
+            Korido = korido;
+            Kor = kor;
+        }
+
+        public Versenyzo(string[] sor)
+        {
+            Csapat = sor[0];
+            VersenyzoNeve = sor[1];
+            Eletkor = Convert.ToInt32(sor[2]);
+            Palya = sor[3];
+            Korido = TimeSpan.Parse(sor[4]);
+            Kor = Convert.ToInt32(sor[5]);
+        }
+
+        
+    }
+}
